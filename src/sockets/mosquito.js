98f6482bd36276edc5a969830f79afb41daa1240
RicardoCordovilla <ricardocordovillaf@gmail.com>
2023-07-31 13:25:32 -0500

    mqtt post

--- src/sockets/mosquito.js
+++ src/sockets/mosquito.js
@@ -1,24 +1,61 @@
-const registersServices = require('../mvc/registers/registers.services')
+// const registersServices = require('../mvc/registers/registers.services')
 
+const { default: axios } = require('axios')
 const express = require('express')
 const app = express()
 app.use(express.json())
 
 const mqtt = require('mqtt')
 const client = mqtt.connect('ws://broker.emqx.io:8083/mqtt')
+let message = ""
+let jsonMessage
+
+const body = [
+    { station: "ESP1", values: { H: 70 + Math.random().toFixed(3) * 10, T: 20 + Math.random().toFixed(3) * 10 }, createdAt: new Date() },
+    { station: "ESP2", values: { H: 70 + Math.random().toFixed(3) * 10, T: 20 + Math.random().toFixed(3) * 10 }, createdAt: new Date() },
+    { station: "ESP1", values: { H: 70 + Math.random().toFixed(3) * 10, T: 20 + Math.random().toFixed(3) * 10 }, createdAt: new Date() },
+    { station: "ESP2", values: { H: 70 + Math.random().toFixed(3) * 10, T: 20 + Math.random().toFixed(3) * 10 }, createdAt: new Date() },
+    { station: "ESP1", values: { H: 70 + Math.random().toFixed(3) * 10, T: 20 + Math.random().toFixed(3) * 10 }, createdAt: new Date() },
+    { station: "ESP2", values: { H: 70 + Math.random().toFixed(3) * 10, T: 20 + Math.random().toFixed(3) * 10 }, createdAt: new Date() },
+    { station: "ESP1", values: { H: 70 + Math.random().toFixed(3) * 10, T: 20 + Math.random().toFixed(3) * 10 }, createdAt: new Date() },
+    { station: "ESP2", values: { H: 70 + Math.random().toFixed(3) * 10, T: 20 + Math.random().toFixed(3) * 10 }, createdAt: new Date() },
+    { station: "ESP1", values: { H: 70 + Math.random().toFixed(3) * 10, T: 20 + Math.random().toFixed(3) * 10 }, createdAt: new Date() },
+    { station: "ESP2", values: { H: 70 + Math.random().toFixed(3) * 10, T: 20 + Math.random().toFixed(3) * 10 }, createdAt: new Date() },
+    { station: "ESP1", values: { H: 70 + Math.random().toFixed(3) * 10, T: 20 + Math.random().toFixed(3) * 10 }, createdAt: new Date() },
+    { station: "ESP2", values: { H: 70 + Math.random().toFixed(3) * 10, T: 20 + Math.random().toFixed(3) * 10 }, createdAt: new Date() },
+]
 
 client.on('connect', function () {
-    client.subscribe('vite3-notification-test', function (err) {
+    client.subscribe('esp32/test', function (err) {
         if (!err) {
-            client.publish('vite3-notification-test', 'Ricardo 💛')
+            client.publish('esp32/test',
+                JSON.stringify(body)
+                // "82.2 | 23."
+            )
+
         }
+
     })
+    // client.end()
+
 })
 
 client.on('message', async function (topic, message) {
     // message is Buffer
-    // console.log("message recived: ", message.toString())
-    const register = await registersServices.createRegister()
-    // console.log(register)
-    // client.end()
+
+    console.log("message recived: ", message.toString())
+    message = message.toString()
+    jsonMessage = JSON.parse(message)
+    console.log(jsonMessage)
+
+    jsonMessage.forEach(element => {
+        console.log(element.station)
+        const body = element
+        axios.post('https://floricola-api-demo-production.up.railway.app/api/v1/registers',
+            body
+        )
+            .then(response => console.log(response.data))
+            .catch(err => console.log(err))
+    });
+    client.end()
 })
